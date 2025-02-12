fee13bfc0489db87555a66d158b1b4a0f88ac123
RikaCelery <qincai233@gmail.com>
2025-02-12 11:19:13 +0800

    disable COVID

--- cmd/main.go
+++ cmd/main.go
@@ -23,7 +23,7 @@ import (
 	_ "github.com/RicheyJang/PaimengBot/basic/sc"
 
 	// 普通插件
-	_ "github.com/RicheyJang/PaimengBot/plugins/COVID"
+	//_ "github.com/RicheyJang/PaimengBot/plugins/COVID"  //
 	_ "github.com/RicheyJang/PaimengBot/plugins/admin"
 	//_ "github.com/RicheyJang/PaimengBot/plugins/bilibili" //disable
 	_ "github.com/RicheyJang/PaimengBot/plugins/bottle"
