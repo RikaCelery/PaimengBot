5061b048fa2ad998a1672f9e14db33f1632a3c9b
RikaCelery <qincai233@gmail.com>
2025-02-22 23:19:46 +0800

    helper function `DownloadToFileHash`

--- utils/client/tools.go
+++ utils/client/tools.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"io"
 	"os"
 )
@@ -25,4 +27,23 @@ func (c *HttpClient) DownloadToFile(filename, url string) error {
 	defer reader.Close()
 	_, err = io.Copy(f, reader)
 	return err
+}
+
+
+// DownloadToFile 下载文件，并返回其绝对路径
+func (c *HttpClient) DownloadToFileHash(filename, url string) (string,error) {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	reader, err := c.GetReader(url)
+	if err != nil {
+		return "", err
+	}
+	defer reader.Close()
+	hasher := md5.New()
+	var dst = io.MultiWriter(f, hasher)
+	_, err = io.Copy(dst, reader)
+	return hex.EncodeToString(hasher.Sum(nil)), err
 }
