d88f5fa570de6846e972d7ce28574f0214c5b752
RikaCelery <qincai233@gmail.com>
2025-03-03 12:52:31 +0800

    fix: images.go 修正 `UseFont`

--- utils/images/images.go
+++ utils/images/images.go
@@ -102,9 +102,9 @@ func (img *ImageCtx) UseDefaultFont(size float64) error {
 	return img.SetFont(GetDefaultFont(), size)
 }
 
-// UseDefaultFont 使用默认字体并设置字体大小
+// UseFont 使用字体并设置字体大小
 func (img *ImageCtx) UseFont(path string, size float64) error {
-	font, err := ParseFont(consts.DefaultTTFPath)
+	font, err := ParseFont(path)
 	if err != nil {
 		return err
 	}
