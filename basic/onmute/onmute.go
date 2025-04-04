d8d5db86071161f8f9c0f098fbb085c8ca31a20e
rikahil <qincai233@gmail.com>
2025-04-04 23:27:50 +0800

    fix: <onmute> 只在自己被禁言时发送提醒

--- basic/onmute/onmute.go
+++ basic/onmute/onmute.go
@@ -10,13 +10,8 @@ import (
 
 var proxy *manager.PluginProxy
 var info = manager.PluginInfo{
-	Name: "禁言提醒",
-	Usage: `再禁言时通知
-config-plugin配置项：
-	event.greeting_msg: 加好友后的欢迎消息 留空不发送
-	event.join_msg: 加群的介绍消息 留空不发送
-	event.notautoleave: 是(true)否(false)关闭被动拉群时自动退群
-	event.autoagree: 是(true)否(false)自动同意所有好友请求`,
+	Name:        "禁言提醒",
+	Usage:       `在禁言时通知`,
 	IsPassive:   true,
 	IsSuperOnly: true,
 	Classify:    "内置功能",
@@ -31,5 +26,7 @@ func init() {
 }
 
 func handleBotMute(ctx *zero.Ctx) {
-	ctx.SendPrivateMessage(ctx.Event.OperatorID, fmt.Sprintf("您好不需要的功能可以关闭（ %s关闭 功能名）或（mikaku/禁用 某个功能），烦请不要禁言", zero.BotConfig.CommandPrefix))
+	if ctx.Event.UserID == ctx.Event.SelfID {
+		ctx.SendPrivateMessage(ctx.Event.OperatorID, fmt.Sprintf("您好不需要的功能可以关闭（ %s关闭 功能名）或（mikaku/禁用 某个功能），烦请不要禁言", zero.BotConfig.CommandPrefix))
+	}
 }
